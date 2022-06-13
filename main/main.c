0719de6dc42c07ef5222a3d26b2ae8e8ee402028
Belal-Elshinnawey <belalshinnawey@gmail.com>
2022-06-13 15:23:02 +0300

    fixing filter issues

--- main/main.c
+++ main/main.c
@@ -24,13 +24,12 @@
 #define LEDC_TIMER LEDC_TIMER_0
 #define LEDC_MODE LEDC_LOW_SPEED_MODE
 #define LEDC_OUTPUT_IO (16) // Define the output GPIO
-#define LEDC_OUTPUT_IO2 (17) // Define the output GPIO
 #define LEDC_CHANNEL LEDC_CHANNEL_0
 #define LEDC_DUTY_RES LEDC_TIMER_10_BIT// Set duty resolution to 13 bits
-#define LEDC_DUTY (84)                // Set duty to 50%. ((2 ** 13) - 1) * 25% = 2048
+#define LEDC_DUTY (16)                // Set duty to 50%. ((2 ** 13) - 1) * 25% = 2048
 #define LEDC_FREQUENCY (20500)           // Frequency in Hertz.
 
-#define TIMES              2048
+#define TIMES              512
 #define GET_UNIT(x)        ((x>>3) & 0x1)
 
 #define ADC_RESULT_BYTE     2
@@ -49,7 +48,7 @@
 #define ACK_CHECK_EN true         /*!< I2C master will check ack from slave*/
 #define ACK_CHECK_DIS false       /*!< I2C master will not check ack from slave */
 #define ACK_VAL 0x0               /*!< I2C ack value */
-#define NACK_VAL 0x1              /*!< I2C nack value */
+#define NACK_VAL 0x1              /*!< I2C nack value */ 
 static uint8_t SEG34_ADDR = 0x48; /*!< Slave address of the 3RD and 4TH 7 Segments 24*/
 static uint8_t SEG12_ADDR = 0x40; /*!< Slave address of the 5th and 6TH 7 Segments 20 */
 
@@ -133,119 +132,80 @@ uint8_t evenbyte[10]= {
 FIR filter designed with
 http://t-filter.appspot.com
 
-sampling frequency: 10000 Hz
+sampling frequency: 80000 Hz
 
 fixed point precision: 16 bits
 
-* 0 Hz - 100 Hz
+* 0 Hz - 19000 Hz
+  gain = 0
+  desired attenuation = -40 dB
+  actual attenuation = n/a
+
+* 20000 Hz - 21000 Hz
   gain = 1
   desired ripple = 5 dB
   actual ripple = n/a
 
-* 300 Hz - 5000 Hz
+* 22000 Hz - 40000 Hz
   gain = 0
   desired attenuation = -40 dB
   actual attenuation = n/a
 
 */
 
-#define FILTER_TAP_NUM 93
-
+#define FILTER_TAP_NUM 47
 static int filter_taps[FILTER_TAP_NUM] = {
-  -86,
-  -31,
-  -36,
-  -41,
-  -45,
-  -49,
-  -52,
-  -54,
-  -55,
-  -54,
-  -51,
-  -47,
-  -40,
-  -31,
-  -19,
-  -4,
-  14,
-  35,
-  59,
-  87,
-  118,
-  152,
-  190,
-  231,
+  151,
+  80,
+  -163,
+  10,
+  136,
+  33,
+  -218,
+  11,
+  245,
+  -16,
+  -310,
+  56,
+  357,
+  -91,
+  -415,
+  144,
+  466,
+  -203,
+  -516,
   275,
-  321,
-  370,
-  421,
-  474,
-  528,
-  582,
-  638,
-  693,
-  747,
-  800,
-  851,
-  900,
-  946,
-  989,
-  1028,
-  1063,
-  1093,
-  1118,
-  1138,
-  1153,
-  1161,
-  1164,
-  1161,
-  1153,
-  1138,
-  1118,
-  1093,
-  1063,
-  1028,
-  989,
-  946,
-  900,
-  851,
-  800,
-  747,
-  693,
-  638,
-  582,
-  528,
-  474,
-  421,
-  370,
-  321,
-  275,
-  231,
-  190,
-  152,
-  118,
-  87,
-  59,
-  35,
-  14,
-  -4,
-  -19,
-  -31,
-  -40,
-  -47,
-  -51,
-  -54,
-  -55,
-  -54,
+  559,
+  -354,
+  -595,
+  442,
+  620,
+  -537,
+  -634,
+  636,
+  635,
+  -738,
+  -622,
+  839,
+  594,
+  -937,
+  -551,
+  1030,
+  494,
+  -1113,
+  -425,
+  1186,
+  343,
+  -1245,
+  -252,
+  1288,
+  154,
+  -1315,
   -52,
-  -49,
-  -45,
-  -41,
-  -36,
-  -31,
-  -86
+  1324,
 };
+
+
 
 
 
@@ -521,6 +481,10 @@ int32_t FIRFilter_calc(FIRFilter *fir, int32_t inputVal){
     //Increase the buffer index. retrun to zero if it reach the end of the index (circular buffer)
     fir->buffIndex++;
     uint8_t sumIndex=fir->buffIndex;
+    uint8_t sumIndex_inv = fir->buffIndex+1;
+    if (sumIndex_inv > FILTER_TAP_NUM-1) {
+        sumIndex_inv = 0;
+    }
     if(fir->buffIndex==FILTER_TAP_NUM){
         fir->buffIndex=0;
     }
@@ -530,16 +494,20 @@ int32_t FIRFilter_calc(FIRFilter *fir, int32_t inputVal){
     int32_t acc  = 0;
     for(int i=0;i<FILTER_TAP_NUM;i++){
         //decrese sum index and warp it if necessary
+        if (sumIndex_inv > FILTER_TAP_NUM-1) {
+            sumIndex_inv = 0;
+        }
         if(sumIndex>0){
             sumIndex--;
         }
         else{
             sumIndex=FILTER_TAP_NUM-1;
         }
+        acc = acc +(((fir->buff[sumIndex]+fir->buff[sumIndex_inv])>>1)*filter_taps[i]);
+        sumIndex_inv++;
         //The convolution process: Multyply the impulse response with the SHIFTED input sample and add it to the output
-        acc = acc +fir->buff[sumIndex]*filter_taps[i];
     }
-    fir->out= acc;
+    fir->out= acc<<1;
     //return the filtered data
     return out;
 
@@ -572,20 +540,6 @@ void app_main(void)
     ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
     ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
 
-    // Prepare and then apply the LEDC PWM channel configuration
-    ledc_channel_config_t ledc_channel2 = {
-        .gpio_num = LEDC_OUTPUT_IO2,
-        .speed_mode = LEDC_MODE,
-        .channel = LEDC_CHANNEL_1,
-        .intr_type = LEDC_INTR_DISABLE,
-        .timer_sel = LEDC_TIMER,
-        .duty = 0,
-        .hpoint = 0};
-    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel2));
-    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL_1, LEDC_DUTY));
-    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL_1));
-
-
     esp_err_t ret;
     uint32_t ret_num = 0;
     uint8_t result[TIMES] = {0};
@@ -604,6 +558,10 @@ void app_main(void)
     int32_t out5=0;
     int32_t out6=0;
     int32_t out7=0;
+    int32_t outOld0=0;
+    int32_t outRateOld0=0;
+
+
     int32_t avg1 =0;
     int32_t avg2 = 0;    
 
@@ -654,12 +612,12 @@ void app_main(void)
                 case 5:
                     FIRFilter_calc(&fir5, (int32_t)p->type1.data);
                     out5 = abs(fir5.out);
-                    //out5 = (int32_t)p->type1.data;
+                    //out6 = (int32_t)p->type1.data;
                     break;
                 case 6:
                     FIRFilter_calc(&fir6, (int32_t)p->type1.data);
                     out6 = abs(fir6.out);
-                    //out6 = (int32_t)p->type1.data;
+                    //out5 = (int32_t)p->type1.data;
                     break;
                 case 7:
                     FIRFilter_calc(&fir7, (int32_t)p->type1.data);
@@ -673,7 +631,11 @@ void app_main(void)
             }
             //avg1 = (out0 + out3 + out6)/3;
             //avg2 = (out5 + out4 + out7)/3;
-            printf("lowerlimit:0,Channel0:%d,Channel3:%d,Channel4:%d,Channel5:%d,Channel6:%d,Channel7:%d,upperlimit:1.6e8\n",out0, out3, out6, out5, out4, out7);
+            
+            //printf("lowerlimit:0,Channel0:%d,Channel3:%d,Channel4:%d,Channel5:%d,Channel6:%d,Channel7:%d,upperlimit:4500000\n",out0, out3, out6, out5, out4, out7);
+            printf("lowerlimit:0,Channel0:%d,Channel0Rate:%d,Channel0Rate2:%d,upperlimit:4500000\n",out3,( out3 - outOld0)<<4, ( out3 - outOld0 -outRateOld0)<<4);
+            outRateOld0 = out3 - outOld0;
+            outOld0 = out3;
             //printf("AVG1: %d, AVG2: %d\n",avg1, avg2);
             //avg1 = 99 * avg1 / (36+12);
             //avg2 = 99 * avg2 / (36+12);
